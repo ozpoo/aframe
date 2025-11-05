e65a2fe464a126b6cdc959a3dc9ac5e3a6bcea9a
Eric Andren <ericrandren@gmail.com>
2025-11-05 13:34:35 -0600

    build

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,9 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig(async ({ command }) => ({
   root: 'src',
-  base: '/ar-image-overlay/',
   build: {
     outDir: path.resolve(__dirname, 'dist')
   }
-  // plugins: [
-  //   viteStaticCopy({
-  //     targets: [
-  //       {
-  //         src: 'assets/targets/*',
-  //         dest: 'assets/targets'
-  //       }
-  //     ]
-  //   })
-  // ]
 }))
